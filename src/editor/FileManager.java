4ccdbe594d094a15fedf062eb8b54f516522bc24
Alireza <rostmi83@gmail.com>
2025-04-24 23:49:27 +0330

    fix all four

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,57 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
+
+
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+
+            BufferedReader buffer = null;
+            try {
+                buffer = new BufferedReader(new FileReader(file));
+                String line;
+                while((line = buffer.readLine()) != null){
+                    textArea.append(line + "\n");
+                }
+
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
+        if (textEditor.currentFile == null) {
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File selectedFile = fileChooser.getSelectedFile();
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile))) {
+                    writer.write(textArea.getText());
+                    textEditor.currentFile = selectedFile;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
-        }
-        else{
-            //TODO
+        } else {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(textEditor.currentFile))) {
+                writer.write(textArea.getText());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
+
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+            textArea.setText("");
+            textEditor.currentFile = null;
     }
 }
